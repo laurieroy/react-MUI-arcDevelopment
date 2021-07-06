132b127ac47a6f763701959f651848787a6894f8
lauretteroy <laurieroy.dev@gmail.com>
2021-07-06 10:00:39 -0700

    Add Header, styles

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import { ThemeProvider } from '@material-ui/styles';
+
+import theme from './ui/Theme';
+import Header from '../components/ui/Header';
+
+function App() {
+  return <ThemeProvider theme={theme}><Header />Hello</ThemeProvider>;
+}
+
+export default App;
+// div className="App"
---
+++ src/components/ui/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+export default function Header(props) {
+  return (
+    <ElevationScroll {...props}>
+      <AppBar>
+        <Toolbar>Arc Development</Toolbar>
+      </AppBar>
+    </ElevationScroll>
+  );
+}
