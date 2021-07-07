715e121f8f269ed4700937d33721da48fc595b6a
lauretteroy <laurieroy.dev@gmail.com>
2021-07-07 10:08:18 -0700

    Customize Theme color, typography

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import CssBaseline from "@material-ui/core/CssBaseline";
+// import CssBaseline from "@material-ui/core/CssBaseline";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import Box from "@material-ui/core/Box";
-import Container from "@material-ui/core/Container";
+// import Box from "@material-ui/core/Box";
+// import Container from "@material-ui/core/Container";
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -20,12 +22,25 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+  },
+}));
+
 export default function Header(props) {
+  const classes = useStyles();
+
   return (
-    <ElevationScroll {...props}>
-      <AppBar>
-        <Toolbar>Arc Development</Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <>
+      <ElevationScroll>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h3">Arc Development</Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </>
   );
 }
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,14 +1,24 @@
 import { createTheme } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
+
+const arcBlue = "#0B72B9";
+const arcOrange = "#FFBA60";
 
 export default createTheme({
   palette: {
+		common: {
+			blue: `${arcBlue}`,
+			orange: `${arcOrange}`
+		},
     primary: {
-      main: purple[500],
+      main: `${arcBlue}`
     },
     secondary: {
-      main: green[500],
-    },
-  },
+			main: `${arcOrange}`
+		}
+	},
+	typography: {
+		h3: {
+			fontWeight: 300
+		} 
+	}
 });
